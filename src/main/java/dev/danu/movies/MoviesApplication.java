7ffeaf297dc6296cc7bc09daa95d6a61190f2f6d
DanushH <danu.nherath@gmail.com>
2024-09-02 15:30:18 +0530

    Add backend functionality

--- src/main/java/dev/danu/movies/MoviesApplication.java
+++ src/main/java/dev/danu/movies/MoviesApplication.java
@@ -3,6 +3,7 @@ package dev.danu.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MoviesApplication {
 
